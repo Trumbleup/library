3bd03c89f6be38e6789d12e1fd50fe984fa80063
Danny <Dtrumble12@gmail.com>
2020-09-23 23:37:49 -0500

    Added a functionality to remove the selected book from the card list

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
-let myLibrary = [{'title': 'Boo', 'author': 'Doo'}, {'title': 'Doo', 'author': 'Obama'}, {'title': 'Star Wars', 'author': 'Charles'}];
-
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
-
-function addBookToLibrary(title, author) {
-  	const newBook = new Book(title, author);
-  	myLibrary.push(newBook);
-}
+let myLibrary = [];
+let keyCounter = 0;
 
 const container = document.querySelector('.container');
 const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
 	const title = document.getElementById('title').value;
 	const author = document.getElementById('author').value;
-	addBookToLibrary(title, author);
+	const pages = document.getElementById('pages').value;
+	let newKey = keyCounter + 1;
+	keyCounter = newKey;
+	addBookToLibrary(title, author, pages, newKey);
 	addCard();
 	e.preventDefault();
 });
 
-function createCard(book) {
-	const card = document.createElement('div');
-	card.classList.add('card');
-
-	const titleHead = document.createElement('h1');
-	titleHead.classList.add('titleHead');
-	titleHead.innerHTML = book.title;
-
-	const authorHead = document.createElement('h2');
-	authorHead.classList.add('authorHead');
-	authorHead.innerHTML = book.author;
-
-	card.appendChild(titleHead);
-	card.appendChild(authorHead);
-
-	container.appendChild(card);
+function addBookToLibrary(title, author, pages, key) {
+  	const newBook = new Book(title, author, pages, key);
+  	myLibrary.push(newBook);
 }
+
+function Book(title, author, pages, key) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.key = key;
+}
+
+Book.prototype.removeCard = removeCard;
 
 function addCard () {
 	const index = myLibrary.length-1; 
@@ -44,9 +34,48 @@ function addCard () {
 	createCard(latestBook);
 }
 
+function removeCard (e) {
+	const selectedKey = e.target.parentNode.getAttribute('data-key');
+	const refArray = myLibrary;
+	const selectedBook = refArray.find(book => {
+		book.key == selectedKey
+	}); 
+	const selectedCard = document.querySelector(`[data-key='${selectedKey}']`);
+	selectedCard.parentNode.removeChild(selectedCard);
+}
+
+function createCard(book) {
+	const { title, author, pages, key } = book;
+
+	const card = document.createElement('div');
+	card.classList.add('card');
+	card.setAttribute('data-key', key);
+
+	const titleHead = document.createElement('h1');
+	titleHead.classList.add('titleHead');
+	titleHead.innerHTML = title;
+
+	const authorHead = document.createElement('h2');
+	authorHead.classList.add('authorHead');
+	authorHead.innerHTML = author;
+
+	const pagesHead = document.createElement('h3');
+	pagesHead.classList.add('pagesHead');
+	pagesHead.innerHTML = pages;
+
+	const removeButton = document.createElement('button');
+	removeButton.addEventListener('click', (e) => {
+		removeCard(e);
+	})
+
+	const hasRead = document.getElementById('read').checked;
+
+	card.appendChild(titleHead);
+	card.appendChild(authorHead);
+	card.appendChild(pagesHead);
+	card.appendChild(removeButton);
+	container.appendChild(card);
+}
 
 
-myLibrary.forEach(book => {
-	createCard(book);
-})
 
